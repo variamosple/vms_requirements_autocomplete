0eea7e985266aecbf9c928ba5eca0d660f205e73
aolopezeafit <aolopezh@eafit.edu.co>
2023-05-25 08:50:18 -0500

    Size limit

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const HOST = '127.0.0.1';
 
 const VERSION = "1.23.04.11.17"
 
+ 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
-app.use(express.json());
+
 app.use(cors());
 
 app.get('/', async function (req, res, next) {
